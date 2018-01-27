c0b85701566a222c6f08b2203a8965c0afdadaad
yu2 <tuyu0515@gmail.com>
2018-01-27 17:55:16 -0600

    Add files via upload

---
+++ file.js
@@ -0,0 +1,31 @@
+var affixes = [];
+$(function() {
+  $('input[multiple]').change(handleFiles);
+});
+
+function handleFiles() {
+  var fileList = this.files;
+  for (var i = 0; i < fileList.length; i++) {
+    $('.output').append(fileList[i].name + "<br>" + fileList[i].type + "<br>" + fileList[i].size + " bytes<br><br>");
+    reader.readAsText(fileList[i]);
+  }
+}
+  
+var reader = new FileReader();
+reader.onload = function(e) {
+  var contents = e.target.result;
+  var lines = contents.split('\n');
+  for (var i = 0; i < lines.length; i++) {
+    var tabs = lines[i].split('\t');
+    affixes.push(new Affix(tabs[2], tabs[1], tabs[0]));
+    for (var j = 0; j < tabs.length; j++) {
+      $('.output').append(tabs[j] + "<br>");
+    }
+  }
+};
+
+function Affix(token, label, abbreviation) {
+  this.token = token;
+  this.label = label;
+  this.abbreviation = abbreviation;
+}
