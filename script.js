0e3f49338752a736599d230d38298757eeadf97d
yu2 <tuyu0515@gmail.com>
2018-09-05 16:13:46 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
   startTime = null;
   cons = document.querySelector(".console");
-	boxParent = document.querySelector(".boxMother");
-	var inputField = document.querySelector(".inputField");
-	inputField.addEventListener("input", function(e) {
-	  cLog("input registered");
-	  let input = inputField.value;
-	  if (input.length >= 3) {
-	    processInput(input.toLowerCase());
-	  }
-	});
+  boxParent = document.querySelector(".boxMother");
+  var inputField = document.querySelector(".inputField");
+  inputField.addEventListener("input", function(e) {
+    cLog("input registered");
+    let input = inputField.value;
+    if (input.length >= 3) {
+      processInput(input.toLowerCase());
+    }
+  });
 });
 
 var roots = [];
@@ -24,54 +24,54 @@ function handleFiles(files, mode) {
   } else if (mode == 'affixes') {
     pusher = pusher.pAffixes;
   }
-	var i = 0;
-	var numFiles = files.length;
-	doFile(files[i]);
+  var i = 0;
+  var numFiles = files.length;
+  doFile(files[i]);
 
-	//Recusive function that goes through each file in succession
-	function doFile(file) {
-	  var promise = new Promise((resolve, reject) => {
-	    outsideResolve = resolve;
-  		let reader = new FileReader();
-  		reader.onload = function(e) {
-  			let doc = e.target.result;
-  			pusher(doc);
-  		};
-  		reader.readAsText(file);
-	  }).then((msg) => {
-			if (i < numFiles - 1) {
-				i++;
-				doFile(files[i]);
-			}
-			else if (i == numFiles - 1) {
-			  trackPerformance();
-				//downloadBlob(roots);
-			}
-	  });
-	}
-	
+  //Recusive function that goes through each file in succession
+  function doFile(file) {
+    var promise = new Promise((resolve, reject) => {
+      outsideResolve = resolve;
+      let reader = new FileReader();
+      reader.onload = function(e) {
+        let doc = e.target.result;
+        pusher(doc);
+      };
+      reader.readAsText(file);
+    }).then((msg) => {
+      if (i < numFiles - 1) {
+        i++;
+        doFile(files[i]);
+      }
+      else if (i == numFiles - 1) {
+        trackPerformance();
+        //downloadBlob(roots);
+      }
+    });
+  }
+  
 }
 
 function downloadBlob(data) {
-	let blob = new Blob([data], {type: 'text/csv'});
-	let url = URL.createObjectURL(blob);
-	let elem = window.document.createElement('a');
-	elem.href = url;
-	elem.download = 'roots.txt';
-	elem.innerHTML = "Download";
-	document.body.appendChild(elem);
-	console.log(roots);
+  let blob = new Blob([data], {type: 'text/csv'});
+  let url = URL.createObjectURL(blob);
+  let elem = window.document.createElement('a');
+  elem.href = url;
+  elem.download = 'roots.txt';
+  elem.innerHTML = "Download";
+  document.body.appendChild(elem);
+  console.log(roots);
 }
 
 function trackPerformance() {
-	if (!startTime) {
-	  startTime = performance.now();
-	}
-	else {
-	  let perf = performance.now() - startTime;
-	  cLog("Performance: " + perf + "ms");
-		startTime = null;
-	}
+  if (!startTime) {
+    startTime = performance.now();
+  }
+  else {
+    let perf = performance.now() - startTime;
+    cLog("Performance: " + perf + "ms");
+    startTime = null;
+  }
 }
 
 function pPush(m) {
@@ -80,18 +80,18 @@ function pPush(m) {
     target = m;
   }
   function pushRoots(d) {
-		let line = d.split('\n');
+    let line = d.split('\n');
     roots = roots.concat(line);
     //roots = trimIt(roots);
     outsideResolve();
   }
   function pushAffixes(d) {
-		let line = d.split('\n');
-		for (let i = 0; i < line.length; i++) {
-			let lineSplit = line[i].split('\t');
-			affixes.push(lineSplit);
-		}
-		outsideResolve();
+    let line = d.split('\n');
+    for (let i = 0; i < line.length; i++) {
+      let lineSplit = line[i].split('\t');
+      affixes.push(lineSplit);
+    }
+    outsideResolve();
   }
   var api = {
     set: setTarget,
@@ -102,13 +102,13 @@ function pPush(m) {
 }
 
 function downloadList(ele) {
-	let value = ele.innerHTML;
-	var blob = (value == "Download Roots")
-	  ? new Blob([roots.join("\r\n")], {type: 'text/csv'})
-	  :	new Blob([affixes.join("\r\n")], {type: 'text/csv'});
-	let url = URL.createObjectURL(blob);
-	ele.href = url;
-	ele.download = "wordlist.txt";
+  let value = ele.innerHTML;
+  var blob = (value == "Download Roots")
+    ? new Blob([roots.join("\r\n")], {type: 'text/csv'})
+    :  new Blob([affixes.join("\r\n")], {type: 'text/csv'});
+  let url = URL.createObjectURL(blob);
+  ele.href = url;
+  ele.download = "wordlist.txt";
 }
 
 function trimIt(ar) {
@@ -142,39 +142,65 @@ function printCharCodes (str) {
   }
 }
 
+var lastMatch = "";
 function processInput(str) {
-	let found = [];
-	for (let i = 0; i < roots.length; i++) {
-		if (roots[i].startsWith(str)) {
-			found.push(roots[i]);
-		}
-		if (i == roots.length - 1) {
-			populateBoxes(found);
-		}
-	}
+  str = doSubs(str);
+  let found = [];
+  let numFound = 0;
+  // Search through roots until 30 matches are found
+  for (let i = 0; i < roots.length; i++) {
+    if (roots[i].startsWith(str)) {
+      found.push(roots[i]);
+      numFound++;
+    }
+    if (numFound == 30) {
+      lastMatch = found[0];
+      populateBoxes(found);
+      break;
+    }
+    else if (i == roots.length - 1 && numFound === 0) {
+      checkAffixes(str.replace(lastMatch, ""));
+    }
+    else if (i == roots.length - 1) {
+      lastMatch = found[0];
+      populateBoxes(found);
+      break;
+    }
+  }
+  
+  function checkAffixes(str) {
+    let aFound = [];
+    console.log(str);
+    for (let i = 0; i < affixes.length; i++) {
+      if(affixes[i][2].startsWith(str)) {
+        aFound.push(affixes[i][2]);
+      }
+    }
+    console.log(aFound);
+  }
 }
 
 function populateBoxes(fd) {
   // First, clear any existing boxes
-	while (boxParent.lastChild) {
-		boxParent.removeChild(boxParent.lastChild);
-	}
-	
-	if (fd.length === 0) {
-	  // if letter(s) are left over after longest root is matched,
-	  // start searching in affixes[i][2]
-	  // must begin new search, starting from root, every time a new letter is inputted
-	  // no affix found
-	}
-	
-	// Only show max 30 matches, longest first
-	let lim = Math.min(30, fd.length);
-	for (let i = 0; i < lim; i++) {
-		let child = document.createElement("div");
-		child.className = "box";
-		child.innerText = fd[i];
-		boxParent.appendChild(child);
-	}
+  while (boxParent.lastChild) {
+    boxParent.removeChild(boxParent.lastChild);
+  }
+  
+  //if (fd.length === 0) {
+    // if letter(s) are left over after longest root is matched,
+    // start searching in affixes[i][2]
+    // must begin new search, starting from root, every time a new letter is inputted
+    // no affix found
+  //}
+  
+  // Only show max 30 matches, longest first
+  let lim = Math.min(30, fd.length);
+  for (let i = 0; i < lim; i++) {
+    let child = document.createElement("div");
+    child.className = "box";
+    child.innerText = fd[i];
+    boxParent.appendChild(child);
+  }
 }
 
 function cLog(str) {
@@ -189,55 +215,55 @@ function toIPA(ar) {
       let joined = roots.join("\u0099");
       joined = doSubs(joined);
       roots = joined.split("\u0099");
-			resolve();
+      resolve();
     }).then((msg) => {
       trackPerformance();
     });
-    
-    function doSubs(ar) {
-      ar = ar
-				.replace(/\u00F1/gu, "\u0272")
-				.replace(/nk/g, "ng")
-				.replace(/nt/g, "nd")
-				.replace(/np/g, "nb")
-				.replace(/manda/g, "manta")
-				.replace(/hua/g, "wa")
-				.replace(/ch/g, "\u02A7")
-				.replace(/sh/g, "\u0283")
-				.replace(/ll/g, "\u0292")
-				.replace(/(rr|^r)/g, "@")
-				.replace(/ r/g, " @")
-				.replace(/ @/g, " \u0290")
-				.replace(/@/g, " \u0290")
-				.replace(/r/g, "\u027E")
-				.replace(/ce/g, "se")
-				.replace(/ci/g, "si")
-				.replace(/ca/g, "ka")
-				.replace(/co/g, "ko")
-				.replace(/cu/g, "ku")
-				.replace(/qu/g, "k")
-				.replace(/hu/g, "xu")
-				.replace(/gui/g, "gi")
-				.replace(/j/g, "x")
-				.replace(/-y-/g, "i") //none in dataset
-				.replace(/y/g, "j")
-				.replace(/^v/gm, "b")
-				.replace(/ v/g, " b")
-				.replace(/v/g, "\u03B2")
-				.replace(/^z/gm, "s")
-				.replace(/ z/g, " s")
-				.replace(/aj/g, "ai")
-				.replace(/aia/g, "aja")
-				.replace(/aw/g, "au")
-				.replace(/aua/g, "awa")
-				.replace(/gua/g, "wa")
-				.replace(/\u00ED/gu, "i")
-				.replace(/\u00E9/gu, "e")
-				.replace(/\u00E1/gu, "a")
-				.replace(/\u00F3/gu, "o")
-				.replace(/\u00FA/gu, "u");
-			return ar;
-    }
   })();
   // Method 2: Iterate through array
+}
+
+function doSubs(ar) {
+  ar = ar
+    .replace(/\u00F1/gu, "\u0272")
+    .replace(/nk/g, "ng")
+    .replace(/nt/g, "nd")
+    .replace(/np/g, "nb")
+    .replace(/manda/g, "manta")
+    .replace(/hua/g, "wa")
+    .replace(/ch/g, "\u02A7")
+    .replace(/sh/g, "\u0283")
+    .replace(/ll/g, "\u0292")
+    .replace(/(rr|^r)/g, "@")
+    .replace(/ r/g, " @")
+    .replace(/ @/g, " \u0290")
+    .replace(/@/g, "\u0290")
+    .replace(/r/g, "\u027E")
+    .replace(/ce/g, "se")
+    .replace(/ci/g, "si")
+    .replace(/ca/g, "ka")
+    .replace(/co/g, "ko")
+    .replace(/cu/g, "ku")
+    .replace(/qu/g, "k")
+    .replace(/hu/g, "xu")
+    .replace(/gui/g, "gi")
+    .replace(/j/g, "x")
+    .replace(/-y-/g, "i") //none in dataset
+    .replace(/y/g, "j")
+    .replace(/^v/gm, "b")
+    .replace(/ v/g, " b")
+    .replace(/v/g, "\u03B2")
+    .replace(/^z/gm, "s")
+    .replace(/ z/g, " s")
+    .replace(/aj/g, "ai")
+    .replace(/aia/g, "aja")
+    .replace(/aw/g, "au")
+    .replace(/aua/g, "awa")
+    .replace(/gua/g, "wa")
+    .replace(/\u00ED/gu, "i")
+    .replace(/\u00E9/gu, "e")
+    .replace(/\u00E1/gu, "a")
+    .replace(/\u00F3/gu, "o")
+    .replace(/\u00FA/gu, "u");
+  return ar;
 }
